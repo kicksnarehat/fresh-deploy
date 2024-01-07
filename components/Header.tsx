5efa2fbae306229df28d0345f54069e8d45358c6
hlsnate <logus@law.harvard.edu>
2024-01-07 12:48:08 -0500

    more nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,8 @@ export default function Header() {
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/greet/Your Name Here">Greet</a>
+        <a href="/randomuser">Random User</a>
+        <a href="/search">Search</a>
         <a href="/github/kicksnarehat">Github User</a>
         <a href="/api/joke">API: Joke</a>
         <a href="/api/random-uuid">API: Random UUID</a>
